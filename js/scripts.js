3fa7370b9362ef8824e87a1badb0d711ccf91faa
David Miller <hello@davidmiller.io>
2021-06-03 18:11:26 -0400

    update dependencies

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 /*!
-* Start Bootstrap - Shop Homepage v5.0.0 (https://startbootstrap.com/template/shop-homepage)
+* Start Bootstrap - Shop Homepage v5.0.1 (https://startbootstrap.com/template/shop-homepage)
 * Copyright 2013-2021 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
 */
